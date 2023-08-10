4ba2f2ba6a262e6cd17311008b41304030558002
jewel <1261087446@qq.com>
2023-08-10 13:45:05 +0800

    feat: element语言，定投规则，粒度计算

--- README.md
+++ README.md
@@ -1,18 +1,8 @@
-# Vue 3 + TypeScript + Vite
+# coin-parse
 
-This template should help get you started developing with Vue 3 and TypeScript in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+分析不同币种不同定投方式与定投时间，对收益的影响
+![](https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/5d1c2e7ee0f14518b96700a88b3b7681~tplv-k3u1fbpfcp-zoom-1.image)
 
-## Recommended IDE Setup
+![](https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/ed63d327d6cd401d93b6e1dbeee06460~tplv-k3u1fbpfcp-zoom-1.image)
 
-- [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
 
-## Type Support For `.vue` Imports in TS
-
-TypeScript cannot handle type information for `.vue` imports by default, so we replace the `tsc` CLI with `vue-tsc` for type checking. In editors, we need [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin) to make the TypeScript language service aware of `.vue` types.
-
-If the standalone TypeScript plugin doesn't feel fast enough to you, Volar has also implemented a [Take Over Mode](https://github.com/johnsoncodehk/volar/discussions/471#discussioncomment-1361669) that is more performant. You can enable it by the following steps:
-
-1. Disable the built-in TypeScript Extension
-   1. Run `Extensions: Show Built-in Extensions` from VSCode's command palette
-   2. Find `TypeScript and JavaScript Language Features`, right click and select `Disable (Workspace)`
-2. Reload the VSCode window by running `Developer: Reload Window` from the command palette.
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,47 @@
 <script setup lang="ts">
+import zhCn from "element-plus/dist/locale/zh-cn.min.mjs"
 import { getCoinMarket, getCurrency } from "./api"
 import { ref, reactive } from "vue"
-import { ElButton, ElFormItem, ElForm, ElSelect, ElOption, ElDatePicker, ElInput } from "element-plus"
+import { ElButton, ElFormItem, ElForm, ElSelect, ElOption, ElDatePicker, ElInput, ElConfigProvider } from "element-plus"
 import dayjs from "dayjs"
 
 const coinOption = ref()
 const investOption = ref()
 const currencyList = ref<string[]>([])
 const coinList = ref<string[]>(["bitcoin"])
+const investRuleList = ref<any[]>([
+  {
+    value: "month",
+    label: "月定投",
+  },
+  {
+    value: "week",
+    label: "周定投",
+  },
+  {
+    value: "day",
+    label: "日定投",
+  },
+])
 
 const form = reactive({
   coin: "bitcoin",
-  currency: "usd",
+  currency: "cny",
   date: "",
   invest: "",
+  investRule: "month",
 })
 
 const onSubmit = () => {
-  // console.log(dayjs(form.date[0]).unix())
   getCoinData()
 }
 
 const getList = () => {
+  // 法币列表
   getCurrency().then((res: any) => {
     currencyList.value = res
   })
+  // 数据量太大
   // getCoins().then((res: any) => {
   //   coinList.value = res.map((c: any) => c.id)
   // })
@@ -36,8 +53,20 @@ const getCoinData = () => {
     from: dayjs(form.date[0]).unix(),
     to: dayjs(form.date[1]).unix(),
   }).then((res: any) => {
+    // 处理粒度
+    let prices = res.prices // 范围价格
+    const daySub = dayjs(form.date[1]).diff(dayjs(form.date[0]), "days")
+    if (daySub < 90) {
+      prices = prices.filter((_: any, index: any) => index % 24 === 0)
+    }
+    if (form.investRule === "week") {
+      prices = prices.filter((_: any, index: any) => index % 7 === 0)
+    } else if (form.investRule === "month") {
+      const curDay = dayjs(form.date[0]).date()
+      prices = prices.filter((item: any) => dayjs(item[0]).date() === curDay)
+    }
     computeCoinOption(res.prices)
-    computeInvest(res.prices)
+    computeInvest(prices)
   })
 }
 
@@ -46,7 +75,7 @@ const computeCoinOption = (prices: any) => {
   const XData = prices.map((item: any) => dayjs(item[0]).format("YYYY-MM-DD"))
   const YData = prices.map((item: any) => item[1])
   coinOption.value = {
-    animationDuration: 10000,
+    animationDuration: 10000,  // 动画
     title: {
       // 标题
       text: `${form.coin}走势`,
@@ -83,14 +112,16 @@ const computeCoinOption = (prices: any) => {
 // 计算定投
 const computeInvest = (prices: any) => {
   const XData = prices.map((item: any) => dayjs(item[0]).format("YYYY-MM-DD"))
+  // 定投本金
   const YData1 = prices.map((_: any, index: number) => (index + 1) * Number(form.invest))
   let r = 0
+  // 资产
   const YData2 = prices.map((item: any) => {
     r += 1 / item[1]
     return (Number(form.invest) * item[1] * r).toFixed(3)
   })
   investOption.value = {
-    animationDuration: 10000,
+    animationDuration: 10000, // 动画
     title: {
       // 标题
       text: `${form.coin}定投图`,
@@ -145,40 +176,39 @@ const computeInvest = (prices: any) => {
 }
 
 getList()
-
 </script>
 
 <template>
-  <div>
+  <el-config-provider :locale="zhCn">
     <el-form :model="form" label-width="120px" :inline="true">
       <el-form-item label="币种：">
-        <el-select v-model="form.coin" filterable placeholder="please select your coin">
+        <el-select v-model="form.coin" filterable placeholder="请选择">
           <el-option :label="coin" :value="coin" :key="coin" v-for="coin in coinList" />
         </el-select>
       </el-form-item>
       <el-form-item label="法币：">
-        <el-select v-model="form.currency" filterable placeholder="please select your currency">
+        <el-select v-model="form.currency" filterable placeholder="请选择">
           <el-option :label="currency" :value="currency" :key="currency" v-for="currency in currencyList" />
         </el-select>
       </el-form-item>
       <el-form-item label="时间范围：">
-        <el-date-picker
-          v-model="form.date"
-          type="daterange"
-          start-placeholder="Start date"
-          end-placeholder="End date"
-        />
+        <el-date-picker v-model="form.date" type="daterange" start-placeholder="开始时间" end-placeholder="结束时间" />
       </el-form-item>
-      <el-form-item label="每日定投金额：">
+      <el-form-item label="定投规则：">
+        <el-select v-model="form.investRule" filterable placeholder="请选择">
+          <el-option :label="item.label" :value="item.value" :key="item.value" v-for="item in investRuleList" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="定投金额：">
         <el-input v-model="form.invest" />
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="onSubmit">确定</el-button>
       </el-form-item>
     </el-form>
-    <ECharts :option="investOption" theme="light" auto-resize style="width: 100%; height: 400px" />
+    <ECharts :option="investOption" theme="light" auto-resize style="width: 100%; height: 500px" />
     <ECharts :option="coinOption" theme="light" auto-resize style="width: 100%; height: 500px" />
-  </div>
+  </el-config-provider>
 </template>
 
 <style scoped></style>
